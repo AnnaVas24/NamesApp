2cc530a813634373799b431825027a16457e1e63
Anna <anna.vasi4ko@icloud.com>
2022-06-26 19:40:57 +0300

    Initial Commit

---
+++ NamesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NamesApp
+//
+//  Created by Vasichko Anna on 26.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
